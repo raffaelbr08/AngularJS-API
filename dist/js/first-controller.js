d1bb7703bc642270c2c96ec4f51644e5a1cb3643
rafaelbr08 <rafael@cwsoftwares.com.br>
2017-02-24 16:29:56 -0300

    ok

---
+++ dist/js/first-controller.js
@@ -0,0 +1,10 @@
+app.controller('HeaderController',function($scope){
+	var nome = "rafael";
+
+	$scope.nome = "Rafael Brussolo";
+	$scope.iniciado = true;
+
+	$scope.alunos = ['Rafael','Joao','maria','jose']
+		
+
+});
